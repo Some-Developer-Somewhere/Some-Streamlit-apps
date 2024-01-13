d20121e12e248c92223ec8a39281480b80dbba87
Atle Jaabaek <atle.jaabaek@kan.no>
2024-01-13 21:23:16 +0100

    Adding a file upload example

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title("Hello World")
 
-st.write("test")
+uploaded_file = st.file_uploader('File uploader')
+
+if uploaded_file is not None:
+    file_content = uploaded_file.read()
+    file_content = file_content.decode()
+
+    st.text_area("Contents of the file", value=file_content, height=500)
+
